44bc82d30a6040d25d0a7752ca133b55eebfc623
georgia622 <s1295492@students.ncl-coll.ac.uk>
2025-08-06 13:13:12 +0100

    Easy level question array added to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+//Quiz Questions
+const easyQuestions = [
+    {
+       question: "Totoro is a large, fluffy forest spirit.",
+       answer: "true"
+    },
+    {
+       question: "Kiki delivers packages on a flying vacuum cleaner.",
+       answer: "false"
+    },
+    {
+       question: "Ponyo is a goldfish who wants to become human.",
+       answer: "true"
+    },
+    {
+       question: "Spirited Away features a bathhouse for spirits.",
+       answer: "true"
+    },
+    {
+       question: "Jiji is Kiki's talking cat companion.",
+       answer: "true"
+    },
+    {
+       question: "In My Neighbor Totoro, there is a magical Catbus.",
+       answer: "true"
+    },
+    {
+       question: "Howl's Moving Castle can walk on legs.",
+       answer: "true"
+    },
+    {
+       question: "Soot sprites appear in both Spirited Away and My Neighbor Totoro.",
+       answer: "true"
+    },
+    {
+       question: "Studio Ghibli films are all in English originally.",
+       answer: "false"
+    },
+    {
+       question: "Grave of the Fireflies is a comedy.",
+       answer: "false."
+    },
+];
